d9bd53c4f86407a5edfc57bade67aabd1c8b1a5c
Anrijs Jargans <ajargans@gmail.com>
2014-03-08 10:29:54 +0200

    Updated default config

--- config.h
+++ config.h
@@ -7,8 +7,8 @@
 #define STATUS_GPIO GPIOC
 #define STATUS_LED1 GPIO_Pin_9
 #define STATUS_LED2 GPIO_Pin_8
-//#define STATUS_LED3 GPIO_Pin_7
-//#define STATUS_LED4 GPIO_Pin_6
+#define STATUS_LED3 GPIO_Pin_9
+#define STATUS_LED4 GPIO_Pin_8
 
 /* Button to start config mode */
 #define BOOT_CONFIG_GPIO GPIOA
@@ -17,4 +17,8 @@
 /* Button to start NRF mode */
 //#define BOOT_NRF_GPIO GPIOA
 //#define BOOT_NRF_PIN GPIO_Pin_1
+
+/* Enable if used wor windows terminal (will add '\r' after newline)*/
+#define WINDOWS_TERMINAL
+
 #endif
